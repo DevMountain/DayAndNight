30eff10b7ac95cd94093206dfe418ecb297a5a98
Joshua Howland <development@jkhowland.com>
2014-06-05 00:30:57 -0600

    Initial Commit

---
+++ DayAndNight/DNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DNAppDelegate.h
+//  DayAndNight
+//
+//  Created by Joshua Howland on 6/5/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
