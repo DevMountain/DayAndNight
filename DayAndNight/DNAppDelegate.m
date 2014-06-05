01359b17ff678592ff602a97616ca4558f86f47d
Joshua Howland <development@jkhowland.com>
2014-06-05 00:33:48 -0600

    Adds view controller as rootviewcontroller of window

--- DayAndNight/DNAppDelegate.m
+++ DayAndNight/DNAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "DNAppDelegate.h"
+#import "DNModeViewController.h"
 
 @implementation DNAppDelegate
 
@@ -14,6 +15,9 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[DNModeViewController new]];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ DayAndNight/DNModeViewController.h
@@ -0,0 +1,13 @@
+//
+//  DNModeViewController.h
+//  DayAndNight
+//
+//  Created by Joshua Howland on 6/5/14.
+//  Copyright (c) 2014 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DNModeViewController : UIViewController
+
+@end
